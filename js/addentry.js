b3c5da15335e340a3a434ca9aa612fdc375eb5e8
rafapaezbas <rpaezbas@rpaezbas.com>
2023-06-26 10:41:20 +0200

    validate hypercore key on add-entry

--- js/addentry.js
+++ js/addentry.js
@@ -1,21 +1,37 @@
 import { html } from 'htm/preact'
 import { useState } from 'preact/hooks'
+import Id from 'hypercore-id-encoding'
 
 function AddEntry (props) {
   const [key, setKey] = useState(null)
   const [type, setType] = useState('core') // default value
   const [description, setDescription] = useState(null)
   const [seeders, setSeeders] = useState(false)
+  const [error, setError] = useState(false)
 
   const createEntry = () => {
-    props.bee.put(key, { description, type, seeders })
-    props.setEntries(e => [...e, { key, value: { description, type, seeders } }])
-    props.setView('main')
+    if (!error && key) {
+      props.bee.put(key, { description, type, seeders })
+      props.setEntries(e => [...e, { key, value: { description, type, seeders } }])
+      props.setView('main')
+    }
+  }
+
+  const onKeyChange = (key) => {
+    try {
+      Id.encode(Buffer.from(key, 'hex'))
+      setError(false)
+      setKey(key)
+    } catch (e) {
+      console.log(e)
+      setError(true)
+    }
   }
 
   return html`
    <div id="add-form">
-     <input id="public-key" type="text" spellcheck="false" placeholder="Public key" onchange=${(e) => setKey(e.target.value)}/>
+      <p class="${error ? 'key-error' : 'disabled'}"> Invalid Hypercore key </p>
+     <input id="public-key" type="text" spellcheck="false" placeholder="Public key" oninput=${(e) => onKeyChange(e.target.value)}/>
      <input id="description" type="text" spellcheck="false" placeholder="Description" onchange=${(e) => setDescription(e.target.value)}/>
      <select id="select-type" onchange=${(e) => setType(e.target.value)}>
        <option value="core">core</option>
@@ -23,7 +39,7 @@ function AddEntry (props) {
        <option value="drive">drive</option>
      </select>
      <label class="seeders-checkbox">Seeders<input type="checkbox" class="checkbox" onchange=${(e) => setSeeders(e.target.checked)}/></label>
-     <p id="add-button" class="${key ? 'enabled-button' : 'disabled-button'}" onclick=${createEntry}>Add Entry</p>
+     <p id="add-button" class="${!error && key ? 'enabled-button' : 'disabled-button'}" onclick=${createEntry}>Add Entry</p>
    </div>
 `
 }
